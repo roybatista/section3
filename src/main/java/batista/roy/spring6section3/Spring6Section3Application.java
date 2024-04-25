99ffb5260191bf72473888e3d33cc39dfc529273
roybatista@gmail.com <adalmastabertoliliano@gmail.com>
2024-04-25 11:14:12 +0100

    initial commit

---
+++ src/main/java/batista/roy/spring6section3/Spring6Section3Application.java
@@ -0,0 +1,13 @@
+package batista.roy.spring6section3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6Section3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6Section3Application.class, args);
+	}
+
+}
---
+++ src/test/java/batista/roy/spring6section3/Spring6Section3ApplicationTests.java
@@ -0,0 +1,13 @@
+package batista.roy.spring6section3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6Section3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
